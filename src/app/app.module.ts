3c00aaf71f75aea913ad19f535613586081c6efb
Camila Farelo <mfarelo92@gmail.com>
2019-11-10 19:25:04 -0500

    STYLES: navbar, variables file, spacing file, add flexgrid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CdComputoComponent } from './cd-computo/cd-computo.component';
+import { NavbarComponent } from './cd-computo/cd-computo.navbar';
 import {HttpClientModule} from '@angular/common/http';
 import {ProductoService} from './services/serviceRest';
 
@@ -13,7 +14,7 @@ import {ProductoService} from './services/serviceRest';
   declarations: [
     AppComponent,
     CdComputoComponent,
-    
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cd-computo/cd-computo.navbar.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nabvar-component',
+  templateUrl: './cd-computo.navbar.html',
+})
+export class NavbarComponent implements OnInit {
+  ngOnInit() {
+    console.log('NavbarComponent');
+  }
+};
+
+
