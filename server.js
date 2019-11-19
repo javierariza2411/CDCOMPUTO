8bb4f24fff6a8e12a554cc40f04ac0e912079dc3
javierariza2411 <javierariza2411@gmail.com>
2019-11-19 14:48:39 -0500

    Actualizacion

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 8088
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/CDCOMPUTO'));
@@ -14,4 +15,6 @@ res.sendFile(path.join(__dirname+'/dist/CDCOMPUTO/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8088);
+app.listen(port, function(){
+    console.log('Servidor iniciado '+port)
+});
