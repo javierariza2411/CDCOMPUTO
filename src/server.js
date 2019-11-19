66ea7d57ced3b593d1f5c6bd3e7b080208611569
javierariza2411 <javierariza2411@gmail.com>
2019-11-19 09:48:38 -0500

    Actualizacion

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
+
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/CDCOMPUTO_V1'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/CDCOMPUTO_V1/index.html'));
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
