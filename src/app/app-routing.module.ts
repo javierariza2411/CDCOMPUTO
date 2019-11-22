4c70d420f16f885b5026d0efc63c2797df39cf7a
Jariza2411 <54323121+Jariza2411@users.noreply.github.com>
2019-11-22 06:11:09 -0500

    carrousel y carro de compras

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CdComputoComponent } from './cd-computo/cd-computo.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:CdComputoComponent}
+  // {path: 'contact', component:''},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
