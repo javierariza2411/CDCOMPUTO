93bab58c7fad2e4932eae78f09d7514b2b5626d2
cesar <cariza@technisupport.com>
2020-05-01 16:02:24 -0500

    distribucion y eliminacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import {
-  MatToolbarModule,  
+  MatToolbarModule,
   MatTabsModule,
   MatButtonModule,
   MatIconModule,
   MatCardModule
 } from '@angular/material';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import { NgModule , CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CdComputoComponent } from './cd-computo/cd-computo.component';
-import { NavbarComponent } from './cd-computo/ui-ux/cd-computo.navbar';
-import { HeaderCarouselComponent } from './cd-computo/ui-ux/cd-computo.header-carousel';
-import { SideNavComponent } from './cd-computo/ui-ux/cd-computo.side-nav';
-import { FooterComponent } from './cd-computo/ui-ux/cd-computo.footer';
-import { SubscriptionListComponent } from './cd-computo/ui-ux/cd-computo.subscription-list';
-import { ContactUsComponent } from './cd-computo/ui-ux/cd-computo.contact-us';
-import {HttpClientModule} from '@angular/common/http';
-import {ProductoService} from './services/serviceRest';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductoService } from './services/serviceRest';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { OpentoggleComponent } from './opentoggle/opentoggle.component';
-import { CarritoComponent} from './carrito/carrito.component';
 import { CarritoService } from './service/carrito.service';
 import { AutocompleteComponent } from './autocomplete/autocomplete.component';
-import { CatalogoComponent} from './catalogo/catalogo.component';
 import { NgxDropzoneModule } from 'ngx-dropzone';
-import { ShoppingCarComponent } from './cd-computo/ui-ux/cd-computo.shopping-car';
-import {UploadFileComponent} from './upload-file/upload-file.component';
-import {ImageUploadModule} from 'angular2-image-upload';
+import { UploadFileComponent } from './upload-file/upload-file.component';
+import { ImageUploadModule } from 'angular2-image-upload';
+import { NavbarComponent } from './componentes/navbar/cd-computo.navbar';
+import { FooterComponent } from './componentes/footer/cd-computo.footer';
+import { SubscriptionListComponent } from './componentes/subscriptions/cd-computo.subscription-list';
+import { ContactUsComponent } from './contactenos/cd-computo.contact-us';
+import { ShoppingCarComponent } from './carrito/cd-computo.shopping-car';
+import { HeaderCarouselComponent } from './componentes/carousel/cd-computo.header-carousel';
 
 @NgModule({
   declarations: [
@@ -41,11 +38,8 @@ import {ImageUploadModule} from 'angular2-image-upload';
     HeaderCarouselComponent,
     FooterComponent,
     SubscriptionListComponent,
-    SideNavComponent,
     OpentoggleComponent,
-    CarritoComponent,
     AutocompleteComponent,
-    CatalogoComponent,
     ContactUsComponent,
     UploadFileComponent,
     ShoppingCarComponent,
@@ -54,7 +48,7 @@ import {ImageUploadModule} from 'angular2-image-upload';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule, 
+    BrowserAnimationsModule,
     MatToolbarModule,
     MatTabsModule,
     MatButtonModule,
@@ -67,8 +61,8 @@ import {ImageUploadModule} from 'angular2-image-upload';
     NgxDropzoneModule,
     ImageUploadModule.forRoot()
   ],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-  providers: [ProductoService,CarritoService],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [ProductoService, CarritoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
